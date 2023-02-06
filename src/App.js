3c996b9388cc045af6de703434553034c5cd6115
danghung <hungdang02042003@gmail.com>
2023-02-06 12:51:24 +0700

    lý thuyết generator function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 
+import React, { useEffect } from 'react';
+
 import ButtonToggle from './components/ButtonToggle';
-import React from 'react';
+import HackerNews from './components/HackerNews';
 import Sidebar from './components/Sidebar';
 
 const App = () => {
-	return (
-		<div>
-			<Sidebar />
-			<ButtonToggle />
-		</div>
-	);
+	// Generator function
+	useEffect(() => {
+		function* demoGenerator() {
+			yield 1;
+			yield 2;
+			yield 3;
+		}
+		const gen = demoGenerator();
+		console.log(gen.next());
+		console.log(gen.next());
+	}, []);
+	return <div>{/* <HackerNews /> */}</div>;
 };
 
 export default App;
--- src/redux-toolkit/configStore.js
+++ src/redux-toolkit/configStore.js
@@ -1,14 +1,17 @@
 import counterSlice, { incrementByValue } from './counterSlice';
 
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from '@redux-saga/core';
 import globalSlice from './globalSlice';
 import logger from 'redux-logger';
+
+const sagaMiddlware = createSagaMiddleware();
 
 // hight order component
 const loggerMiddleware = (store) => (next) => (action) => {
 	action.payload = 10; // can thiệp gán giá trị mới cho payload
 	next(action);
-	console.log('🚀 ~ file: configStore.js:17 ~ action', action);
+	// console.log('🚀 ~ file: configStore.js:17 ~ action', action);
 };
 
 export const store = configureStore({
@@ -16,14 +19,16 @@ export const store = configureStore({
 		counter: counterSlice,
 		global: globalSlice,
 	},
-	middleware: (gDM) => gDM().concat(logger),
+	middleware: (gDM) => gDM().concat(logger, sagaMiddlware),
 });
+
+// sagaMiddlware.run(rootSaga)
 
 // store.subscribe(() => {
 // 	// javascript observer pattern
 // 	console.log(`currentState: ${store.getState().counter.count}`);
 // });
 
-store.dispatch(incrementByValue(1));
+// store.dispatch(incrementByValue(1));
 // store.dispatch(incrementByValue(3));
 // store.dispatch(incrementByValue(5));
